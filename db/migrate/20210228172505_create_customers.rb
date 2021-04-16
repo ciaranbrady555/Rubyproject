85d86f0f0cf436b8b5ca9335de790dcd5dffc28b
ciaranbrady555 <ciaranbrady1060@gmail.com>
2021-04-16 18:20:17 +0100

    final push
    As far as I could get I wish I could have finished it properly

--- db/migrate/20210228172505_create_customers.rb
+++ db/migrate/20210228172505_create_customers.rb
@@ -3,11 +3,13 @@ class CreateCustomers < ActiveRecord::Migration[6.1]
     create_table :customers do |t|
       t.string :first_name
       t.string :last_name
-      t.string :car_license
-      t.integer :phone
+      t.string :plate
+      t.integer:phone
       t.string :email
-
       t.timestamps
+
+
+      
     end
   end
 end
